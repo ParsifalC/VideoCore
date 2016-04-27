ef488c308007058f91ed2909ffcc2cde58a2cb23
mingweizhang <mingweiz@foxmail.com>
2016-04-27 16:37:09 +0800

    update podfile.fix build warning.

---
+++ sample/SampleBroadcaster-Swift/Pods/Target Support Files/Pods-SampleBroadcaster-Swift/Pods-SampleBroadcaster-Swift-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SampleBroadcaster_SwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SampleBroadcaster_SwiftVersionString[];
+
--- sample/SampleBroadcaster-Swift/Pods/Target Support Files/VideoCore/VideoCore-umbrella.h
+++ sample/SampleBroadcaster-Swift/Pods/Target Support Files/VideoCore/VideoCore-umbrella.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 
-#import "VCPreviewView.h"
-#import "VCSimpleSession.h"
+#import "api/iOS/VCPreviewView.h"
+#import "api/iOS/VCSimpleSession.h"
 
 FOUNDATION_EXPORT double VideoCoreVersionNumber;
 FOUNDATION_EXPORT const unsigned char VideoCoreVersionString[];
